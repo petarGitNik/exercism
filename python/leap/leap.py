d459fed0e10f905d3f0e379b4a74daa06d1f8540
Petar Gitnik <petarGitNik@users.noreply.github.com>
2017-07-15 22:53:54 +0200

    Finish the leap year exercise

--- python/leap/leap.py
+++ python/leap/leap.py
@@ -1,2 +1,19 @@
-def is_leap_year():
-    pass
+def is_leap_year(year):
+    """
+    Find if a year is a leap year.
+    """
+    if year % 4 == 0:
+        if year % 100 != 0:
+            return True
+        else:
+            # If a year is divisible by 100, but ...
+            if year % 400 != 0:
+                return False
+            else:
+                return True
+        if year % 400 == 0:
+            return True
+        return True
+    # If a year is not divisible by 4, the it is not a candidate to
+    # be a leap year
+    return False
