ad8d65a13e7de5bbc0196ee911405dec5506bf0e
Petar Gitnik <petarGitNik@users.noreply.github.com>
2017-07-16 20:41:15 +0200

    Refactor meetup

--- python/meetup/meetup.py
+++ python/meetup/meetup.py
@@ -5,18 +5,14 @@ from calendar import monthrange
 TEENTH_DAYS = list(range(13, 20))
 
 def meetup_day(year, month, dayname, descriptor):
-    #day = 0
     if descriptor == 'teenth':
-        for candidate in TEENTH_DAYS:
-            if date(year, month, candidate).strftime('%A') == dayname:
-                #day = candidate
-                return date(year, month, candidate)
+        return search_seven_days_period(year, month, dayname, descriptor, TEENTH_DAYS)
+
     if descriptor == 'last':
         days = monthrange(year, month)[1]
         last_days = range(days - 7 + 1, days + 1)
-        for candidate in last_days:
-            if date(year, month, candidate).strftime('%A') == dayname:
-                return date(year, month, candidate)
+        return search_seven_days_period(year, month, dayname, descriptor, last_days)
+
     if descriptor[-2:] in ['st', 'nd', 'rd', 'th']:
         enumerator = int(re.findall(r'\d+', descriptor)[0])
         count = 0
@@ -26,15 +22,15 @@ def meetup_day(year, month, dayname, descriptor):
                 if date(year, month, candidate).strftime('%A') == dayname:
                     count += 1
                 if count == enumerator:
-                    #day = candidate
                     return date(year, month, candidate)
                 candidate += 1
         except ValueError:
             raise MeetupDayException('Specified day does not exist.')
 
-    #pass
-    #raise MeetupDayException('Specified day does not exist.')
-    #return date(year, month, day)
+def search_seven_days_period(year, month, dayname, descriptor, period):
+    for candidate in period:
+        if date(year, month, candidate).strftime('%A') == dayname:
+            return date(year, month, candidate)
 
 class MeetupDayException(Exception):
     pass
