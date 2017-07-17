2d23d79d5af0f7bab701134cdce0f11e8b322b74
Petar Gitnik <petarGitNik@users.noreply.github.com>
2017-07-18 00:01:18 +0200

    Solve allergies

--- python/allergies/allergies.py
+++ python/allergies/allergies.py
@@ -1,19 +1,19 @@
 class Allergies(object):
-    # lst = []
 
     ALLERGIES = {
-        'eggs' : 1
-        'peanuts' : 2
-        'shellfish' : 4
-        'strawberries' : 8
-        'tomatoes' : 16
-        'chocolate' : 32
-        'pollen' : 64
-        'cats' : 128
+        'eggs' : 1,
+        'peanuts' : 2,
+        'shellfish' : 4,
+        'strawberries' : 8,
+        'tomatoes' : 16,
+        'chocolate' : 32,
+        'pollen' : 64,
+        'cats' : 128,
     }
 
     def __init__(self, score):
         self.score = score
+        self.lst = [allergy for allergy, grade in self.ALLERGIES.items() if score & grade]
 
-    def is_alergic_to(self, alergy):
-        pass
+    def is_allergic_to(self, allergy):
+        return allergy in self.lst
